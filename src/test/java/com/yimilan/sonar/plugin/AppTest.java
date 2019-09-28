4e61d94ba50dfe2ffbb333261e56dce8dd93994d
zhaogy <413462840@qq.com>
2019-09-28 14:32:48 +0800

    1

---
+++ src/test/java/com/yimilan/sonar/plugin/AppTest.java
@@ -0,0 +1,20 @@
+package com.yimilan.sonar.plugin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
